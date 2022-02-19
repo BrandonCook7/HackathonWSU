4137b4586639eddb71fc76adffd5080861e3adf0
Eric Simpson <52686452+ericssimpson@users.noreply.github.com>
2022-02-19 10:42:54 -0800

    adding events schema

---
+++ models/Event.js
@@ -0,0 +1,15 @@
+const { model, Schema } = require("mongoose");
+
+const userSchema = new Schema({
+  username: { type: String, default: null },
+  email: { type: String, unique: true },
+  password: { type: String },
+  token: { type: String }
+}); module.exports = model("user", userSchema);
+
+const eventSchema = new Schema({
+  //username: { type: String, default: null },
+  //email: { type: String, unique: true },
+  //password: { type: String },
+  //token: { type: String }
+}); module.exports = model("event", eventSchema);
