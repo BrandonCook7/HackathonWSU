14db26f259422ef26176ba06baed982793e2dac8
Eric Simpson <52686452+ericssimpson@users.noreply.github.com>
2022-02-19 10:56:16 -0800

    modifying some eventschema stuff

--- models/Event.js
+++ models/Event.js
@@ -1,7 +1,7 @@
 const { model, Schema } = require("mongoose");
 
 const eventSchema = new Schema({
-  //username: { type: String, default: null },
+  //tags: { type: String, default: null },
   //email: { type: String, unique: true },
   //password: { type: String },
   //token: { type: String }
