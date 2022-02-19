b43e9d4f69fee9476eec1aedc45c9b9209f05356
Brandon Cook <brandoncook7000@gmail.com>
2022-02-19 14:35:26 -0800

    Worked on resolvers.

--- graphql/resolvers/events.js
+++ graphql/resolvers/events.js
@@ -1,83 +1,119 @@
 const User = require('../../models/Event'); //SKELETON FROM USERS
-// const {
-//     ApolloServer,
-//     gql,
-//     UserInputError
-// } = require('apollo-server');
-// const { ApolloError } = require('apollo-server-errors');
-// const jwt = require("jsonwebtoken");
-// const bcrypt = require("bcryptjs");
+const {
+    ApolloServer,
+    gql,
+    UserInputError
+} = require('apollo-server');
+const { ApolloError } = require('apollo-server-errors');
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcryptjs");
 
-// module.exports = {
-//     Mutation: {
-//         async registerUser(_, {registerInput: {username, email, password} }) {
-//             /* Do input validation
-//             if (!(email && password && first_name && last_name)) {
-//                 res.status(400).send("All input is required");
-//             }
-//             */
-//             const oldUser = await User.findOne({ email });
+module.exports = {
+    Mutation: {
+        async registerUser(_, {registerInput: {username, email, password} }) {
+            /* Do input validation
+            if (!(email && password && first_name && last_name)) {
+                res.status(400).send("All input is required");
+            }
+            */
+            const oldUser = await User.findOne({ email });
 
-//             if (oldUser) {
-//                 throw new ApolloError('A user is already registered with the email: ' + email, 'USER_ALREADY_EXISTS');
-//             }
+            if (oldUser) {
+                throw new ApolloError('A user is already registered with the email: ' + email, 'USER_ALREADY_EXISTS');
+            }
             
-//             var encryptedPassword = await bcrypt.hash(password, 10);
+            var encryptedPassword = await bcrypt.hash(password, 10);
             
-//             const newUser = new User({
-//                 username: username,
-//                 email: email.toLowerCase(),
-//                 password: encryptedPassword
-//             });
+            const newUser = new User({
+                username: username,
+                email: email.toLowerCase(),
+                password: encryptedPassword
+            });
 
-//             const token = jwt.sign(
-//                 { user_id: newUser._id, email },
-//                 "UNSAFESTRING",
-//                 {
-//                   expiresIn: "2h",
-//                 }
-//             );
+            const token = jwt.sign(
+                { user_id: newUser._id, email },
+                "UNSAFESTRING",
+                {
+                  expiresIn: "2h",
+                }
+            );
 
-//             newUser.token = token;
+            newUser.token = token;
 
-//             const res = await newUser.save();
+            const res = await newUser.save();
             
-//             return {
-//                 id: res.id,
-//                 ...res._doc
-//             };
-//         },
-//         async loginUser(_, {loginInput: {email, password} }) {
-//             /* Do input validation
-//             if (!(email && password)) {
-//                 res.status(400).send("All input is required");
-//             }
-//             */
-//             const user = await User.findOne({ email });
+            return {
+                id: res.id,
+                ...res._doc
+            };
+        },
+        async loginUser(_, {loginInput: {email, password} }) {
+            /* Do input validation
+            if (!(email && password)) {
+                res.status(400).send("All input is required");
+            }
+            */
+            const user = await User.findOne({ email });
 
-//             if (user && (await bcrypt.compare(password, user.password))) {
-//                 // Create token
-//                 const token = jwt.sign(
-//                   { user_id: user._id, email },
-//                   "UNSAFESTRING",
-//                   {
-//                     expiresIn: "2h",
-//                   }
-//                 );
+            if (user && (await bcrypt.compare(password, user.password))) {
+                // Create token
+                const token = jwt.sign(
+                  { user_id: user._id, email },
+                  "UNSAFESTRING",
+                  {
+                    expiresIn: "2h",
+                  }
+                );
           
-//                 // save user token
-//                 user.token = token;
+                // save user token
+                user.token = token;
 
-//                 return {
-//                     id: user.id,
-//                     ...user._doc
-//                 }
-//             } else {
-//                 throw new ApolloError('Incorrect password', 'INCORRECT_PASSWORD');
-//             }
-//         }
-//     },
-//     Query: {
-//         user: (_, {ID}) => User.findById(ID)
-//     }
-// }
+                return {
+                    id: user.id,
+                    ...user._doc
+                }
+            } else {
+                throw new ApolloError('Incorrect password', 'INCORRECT_PASSWORD');
+            }
+        }
+    },
+    Query: {
+        event: (_, {ID}) => Event.findById(ID),
+        async getEvents(_, {limit}) {//Get latest events with limit
+            const events = await Event.find({}).sort({created: -1}).limit(limit);
+            return events.map(event => {
+                return {
+                    id: event.id,
+                    ...event._doc
+                }
+            });
+        },
+        async getEventsByTag(_, {tag, limit}) {//Get latest events by tag with limit
+            const events = await Event.find({tags: tag}).sort({created: -1}).limit(limit);
+            return events.map(event => {
+                return {
+                    id: event.id,
+                    ...event._doc
+                }
+            });
+        },
+        async getEventsByHost(_, {host, limit}) {//Get latest events by host with limit
+            const events = await Event.find({host: host}).sort({created: -1}).limit(limit);
+            return events.map(event => {
+                return {
+                    id: event.id,
+                    ...event._doc
+                }
+            });
+        },
+        async getEventsByKeyword(_, {keyword, limit}) {//Get latest events by keyword and limit
+            const events = await Event.find({name: {$regex: /keyword/, $options: 'i'}}).sort({created: -1}).limit(limit);
+            return events.map(event => {
+                return {
+                    id: event.id,
+                    ...event._doc
+                }
+            });
+        }
+    }
+}
--- graphql/resolvers/tags.js
+++ graphql/resolvers/tags.js
@@ -1,74 +1,74 @@
 const Tag = require('../../models/Tag'); //SKELETON
-// const {
-//     ApolloServer,
-//     gql,
-//     UserInputError
-// } = require('apollo-server');
-// const { ApolloError } = require('apollo-server-errors');
+const {
+    ApolloServer,
+    gql,
+    UserInputError
+} = require('apollo-server');
+const { ApolloError } = require('apollo-server-errors');
 
-// module.exports = {
-//     Mutation: {
-//         async addTag(_, {registerInput: {category, icon, color} }) {
-//             const oldTag = await Tag.findOne({ category });
+module.exports = {
+    Mutation: {
+        async addTag(_, {registerInput: {category, icon, color} }) {
+            const oldTag = await Tag.findOne({ category });
 
-//             if (oldTag) {
-//                 throw new ApolloError('This tag already exists: ' + category, 'TAG_ALREADY_EXISTS');
-//             }
+            if (oldTag) {
+                throw new ApolloError('This tag already exists: ' + category, 'TAG_ALREADY_EXISTS');
+            }
             
-//             const newTag = new Tag({
-//                 category: category.tolowerCase(),
-//                 icon: icon.toLowerCase(),
-//                 color: color.toLowerCase()
-//             });
+            const newTag = new Tag({
+                category: category.tolowerCase(),
+                icon: icon.toLowerCase(),
+                color: color.toLowerCase()
+            });
 
-//             const token = jwt.sign(
-//                 { user_id: newUser._id, email },
-//                 "UNSAFESTRING",
-//                 {
-//                   expiresIn: "2h",
-//                 }
-//             );
+            const token = jwt.sign(
+                { user_id: newUser._id, email },
+                "UNSAFESTRING",
+                {
+                  expiresIn: "2h",
+                }
+            );
 
-//             newUser.token = token;
+            newUser.token = token;
 
-//             const res = await newUser.save();
+            const res = await newUser.save();
             
-//             return {
-//                 id: res.id,
-//                 ...res._doc
-//             };
-//         },
-//         async loginUser(_, {loginInput: {email, password} }) {
-//             /* Do input validation
-//             if (!(email && password)) {
-//                 res.status(400).send("All input is required");
-//             }
-//             */
-//             const user = await User.findOne({ email });
+            return {
+                id: res.id,
+                ...res._doc
+            };
+        },
+        async loginUser(_, {loginInput: {email, password} }) {
+            /* Do input validation
+            if (!(email && password)) {
+                res.status(400).send("All input is required");
+            }
+            */
+            const user = await User.findOne({ email });
 
-//             if (user && (await bcrypt.compare(password, user.password))) {
-//                 // Create token
-//                 const token = jwt.sign(
-//                   { user_id: user._id, email },
-//                   "UNSAFESTRING",
-//                   {
-//                     expiresIn: "2h",
-//                   }
-//                 );
+            if (user && (await bcrypt.compare(password, user.password))) {
+                // Create token
+                const token = jwt.sign(
+                  { user_id: user._id, email },
+                  "UNSAFESTRING",
+                  {
+                    expiresIn: "2h",
+                  }
+                );
           
-//                 // save user token
-//                 user.token = token;
+                // save user token
+                user.token = token;
 
-//                 return {
-//                     id: user.id,
-//                     ...user._doc
-//                 }
-//             } else {
-//                 throw new ApolloError('Incorrect password', 'INCORRECT_PASSWORD');
-//             }
-//         }
-//     },
-//     Query: {
-//         user: (_, {ID}) => User.findById(ID)
-//     }
-// }
+                return {
+                    id: user.id,
+                    ...user._doc
+                }
+            } else {
+                throw new ApolloError('Incorrect password', 'INCORRECT_PASSWORD');
+            }
+        }
+    },
+    Query: {
+        user: (_, {ID}) => User.findById(ID)
+    }
+}
