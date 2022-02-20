978acb277b19925185d5a97301665c76513a3974
Eric Simpson <52686452+ericssimpson@users.noreply.github.com>
2022-02-19 16:51:48 -0800

    updating error return

--- graphql/resolvers/events.js
+++ graphql/resolvers/events.js
@@ -13,7 +13,7 @@ module.exports = {
             const hostUser = await User.findOne({ email: host_email });
 
             if (!hostUser) {
-                throw new ApolloError('No User');
+                throw new ApolloError('User does not exist', 'USER_DOES_NOT_EXISTS');
             }
             else{
                 const addEvent = new Event({
