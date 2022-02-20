eb37cac38b9bf09fbb64a566088fc628f8a4c3b1
Wade Cappa <wade.cappa@msn.com>
2022-02-20 06:02:24 -0800

    fixed dumb issue

--- models/User.js
+++ models/User.js
@@ -6,7 +6,7 @@ const userSchema = new Schema({
   password: { type: String },
   token: { type: String },
   reputation: { type: Number, default: 0},
-  short_description: {type: String},
+  short_description: {type: String, default: ""},
   interests: { type: [String], default: [] }
 });
 
